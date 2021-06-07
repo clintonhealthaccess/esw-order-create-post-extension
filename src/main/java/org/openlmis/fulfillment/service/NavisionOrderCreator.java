41fa640526920d5f8025a6be510d846b5b38576d
Chongsun Ahn <cahn922@yahoo.com>
2021-06-07 16:17:54 -0700

    Process order after creation and send it to external API
    Currently hardcoded to docker host, where something like WireMock can respond to it. Gets order and reference data information. Does not get period end date or request quantity explanation or any other requisition fields, as those require going to the Requisition Service, which will require some more work.

--- src/main/java/org/openlmis/fulfillment/service/NavisionOrderCreator.java
+++ src/main/java/org/openlmis/fulfillment/service/NavisionOrderCreator.java
@@ -15,25 +15,109 @@
 
 package org.openlmis.fulfillment.service;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.openlmis.fulfillment.domain.Order;
+import org.openlmis.fulfillment.domain.OrderLineItem;
 import org.openlmis.fulfillment.extension.point.OrderCreatePostProcessor;
+import org.openlmis.fulfillment.service.referencedata.FacilityDto;
+import org.openlmis.fulfillment.service.referencedata.FacilityReferenceDataService;
+import org.openlmis.fulfillment.service.referencedata.OrderableDto;
+import org.openlmis.fulfillment.service.referencedata.OrderableReferenceDataService;
+import org.openlmis.fulfillment.service.referencedata.ProgramDto;
+import org.openlmis.fulfillment.service.referencedata.ProgramReferenceDataService;
+import org.openlmis.fulfillment.service.request.RequestHeaders;
+import org.openlmis.fulfillment.service.request.RequestHelper;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.slf4j.profiler.Profiler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @Component(value = "NavisionOrderCreator")
 public class NavisionOrderCreator implements OrderCreatePostProcessor {
 
   private static final XLogger XLOGGER = XLoggerFactory.getXLogger(NavisionOrderCreator.class);
 
+  @Autowired
+  private FacilityReferenceDataService facilityReferenceDataService;
+
+  @Autowired
+  private ProgramReferenceDataService programReferenceDataService;
+
+  @Autowired
+  private OrderableReferenceDataService orderableReferenceDataService;
+
+  private final RestOperations restTemplate = new RestTemplate();
+
   @Override
   public void process(Order order) {
     XLOGGER.entry(order);
-    Profiler profiler = new Profiler("DEFAULT_ORDER_CREATE_POST_PROCESSOR");
+    Profiler profiler = new Profiler("NAVISION_ORDER_CREATE_POST_PROCESSOR");
     profiler.setLogger(XLOGGER);
 
-    XLOGGER.info("Testing that we have gotten to the Navision extension point");
+    XLOGGER.debug("Getting reference data to create order to Navision API");
+    FacilityDto supplyingFacility = facilityReferenceDataService
+        .findOne(order.getSupplyingFacilityId());
+
+    ProgramDto program = programReferenceDataService
+        .findOne(order.getProgramId());
+
+    List<UUID> orderableIds = new ArrayList<>();
+    for (OrderLineItem orderLineItem : order.getOrderLineItems()) {
+      orderableIds.add(orderLineItem.getOrderable().getId());
+    }
+    List<OrderableDto> orderables = orderableReferenceDataService
+        .findByIds(orderableIds);
+    Map<UUID, OrderableDto> orderablesMap = orderables.stream()
+        .collect(Collectors.toMap(OrderableDto::getId, Function.identity()));
+
+    XLOGGER.debug("Adding line items to request body");
+    List<NavisionOrderLineItemDto> request = new ArrayList<>();
+    for (OrderLineItem orderLineItem : order.getOrderLineItems()) {
+      NavisionOrderLineItemDto navisionLineItem = new NavisionOrderLineItemDto(
+          supplyingFacility.getCode(),
+          program.getCode(),
+          order.getCreatedDate().toString(),
+          order.getId(),
+          order.getOrderCode(),
+          order.getEmergency(),
+          orderablesMap.get(orderLineItem.getOrderable().getId()).getProductCode(),
+          orderLineItem.getOrderable().getId(),
+          orderablesMap.get(orderLineItem.getOrderable().getId()).getFullProductName(),
+          orderablesMap.get(orderLineItem.getOrderable().getId()).getDispensable().getDisplayUnit(),
+          orderLineItem.getOrderedQuantity(),
+          ""
+      );
+      request.add(navisionLineItem);
+    }
+
+    String url = "http://172.17.0.1:9999/Company('39')/ReportAndRequisition";
+    try {
+      XLOGGER.debug("Sending request");
+      RequestHeaders headers;
+      headers = RequestHeaders.init().setAuth("98c5faf9-05cc-4839-aebc-7677bd916d41");
+      URI uri = RequestHelper.createUri(url);
+      HttpEntity<List<NavisionOrderLineItemDto>> entity = RequestHelper.createEntity(request, headers);
+
+      restTemplate.postForObject(uri, entity, Object.class);
+    } catch (HttpStatusCodeException ex) {
+      XLOGGER.warn(
+          "Unable to create order in Navision. Error code: {}, response message: {}",
+          ex.getStatusCode(), ex.getResponseBodyAsString()
+      );
+    } catch (Exception ex) {
+      XLOGGER.warn("Unable to create order in Navision. Error message: {}", ex.getMessage());
+    }
 
     profiler.stop().log();
     XLOGGER.exit();
---
+++ src/main/java/org/openlmis/fulfillment/service/NavisionOrderLineItemDto.java
@@ -0,0 +1,101 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2021 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation either
+ * version 3 of the License or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.fulfillment.service;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NavisionOrderLineItemDto {
+
+  private String Facility_No;
+  private String Program_Code;
+  private String Period_End_Date;
+  private UUID Order_ID;
+  private String Order_No;
+  private Boolean Emergency;
+  private String Product_Code;
+  private UUID Product_ID;
+  private String Product;
+  private String Dispencing_UOM;
+  private Long Requested_Quantity;
+  private String Requested_Quantity_Explanation;
+
+  @JsonProperty("Facility_No")
+  public String getFacility_No() {
+    return Facility_No;
+  }
+
+  @JsonProperty("Program_Code")
+  public String getProgram_Code() {
+    return Program_Code;
+  }
+
+  @JsonProperty("Period_End_Date")
+  public String getPeriod_End_Date() {
+    return Period_End_Date;
+  }
+
+  @JsonProperty("Order_ID")
+  public UUID getOrder_ID() {
+    return Order_ID;
+  }
+
+  @JsonProperty("Order_No")
+  public String getOrder_No() {
+    return Order_No;
+  }
+
+  @JsonProperty("Emergency")
+  public Boolean getEmergency() {
+    return Emergency;
+  }
+
+  @JsonProperty("Product_Code")
+  public String getProduct_Code() {
+    return Product_Code;
+  }
+
+  @JsonProperty("Product_ID")
+  public UUID getProduct_ID() {
+    return Product_ID;
+  }
+
+  @JsonProperty("Product")
+  public String getProduct() {
+    return Product;
+  }
+
+  @JsonProperty("Dispencing_UOM")
+  public String getDispencing_UOM() {
+    return Dispencing_UOM;
+  }
+
+  @JsonProperty("Requested_Quantity")
+  public Long getRequested_Quantity() {
+    return Requested_Quantity;
+  }
+
+  @JsonProperty("Requested_Quantity_Explanation")
+  public String getRequested_Quantity_Explanation() {
+    return Requested_Quantity_Explanation;
+  }
+}
