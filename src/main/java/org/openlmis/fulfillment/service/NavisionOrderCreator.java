e0d42a03441a664b317dddec5657275ca323283d
Chongsun Ahn <cahn922@yahoo.com>
2021-06-08 10:52:59 -0700

    Add required requisition data and make external API configurable
    Set required requisition fields in request body. This requires creating a new requisition service interface to access requisition APIs. Also change Navision API URL from hardcoded to configurable in a custom profile application.properties file.

--- src/main/java/org/openlmis/fulfillment/service/NavisionOrderCreator.java
+++ src/main/java/org/openlmis/fulfillment/service/NavisionOrderCreator.java
@@ -29,6 +29,8 @@ import org.openlmis.fulfillment.service.referencedata.FacilityDto;
 import org.openlmis.fulfillment.service.referencedata.FacilityReferenceDataService;
 import org.openlmis.fulfillment.service.referencedata.OrderableDto;
 import org.openlmis.fulfillment.service.referencedata.OrderableReferenceDataService;
+import org.openlmis.fulfillment.service.referencedata.PeriodReferenceDataService;
+import org.openlmis.fulfillment.service.referencedata.ProcessingPeriodDto;
 import org.openlmis.fulfillment.service.referencedata.ProgramDto;
 import org.openlmis.fulfillment.service.referencedata.ProgramReferenceDataService;
 import org.openlmis.fulfillment.service.request.RequestHeaders;
@@ -37,6 +39,7 @@ import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.slf4j.profiler.Profiler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -57,7 +60,16 @@ public class NavisionOrderCreator implements OrderCreatePostProcessor {
   @Autowired
   private OrderableReferenceDataService orderableReferenceDataService;
 
+  @Autowired
+  private RequisitionService requisitionService;
+
+  @Autowired
+  private PeriodReferenceDataService periodReferenceDataService;
+
   private final RestOperations restTemplate = new RestTemplate();
+
+  @Value("${navision.url}")
+  private String navisionUrl;
 
   @Override
   public void process(Order order) {
@@ -81,13 +93,20 @@ public class NavisionOrderCreator implements OrderCreatePostProcessor {
     Map<UUID, OrderableDto> orderablesMap = orderables.stream()
         .collect(Collectors.toMap(OrderableDto::getId, Function.identity()));
 
+    XLOGGER.debug("Getting requisition data to create order to Navision API");
+    RequisitionDto requisition = requisitionService.findOne(order.getExternalId());
+    ProcessingPeriodDto processingPeriod = periodReferenceDataService
+        .findOne(requisition.getProcessingPeriod().getId());
+    Map<UUID, RequisitionLineItemDto> lineItemsMap = requisition.getRequisitionLineItems().stream()
+        .collect(Collectors.toMap(lineItem -> lineItem.getOrderable().getId(), lineItem -> lineItem));
+
     XLOGGER.debug("Adding line items to request body");
     List<NavisionOrderLineItemDto> request = new ArrayList<>();
     for (OrderLineItem orderLineItem : order.getOrderLineItems()) {
       NavisionOrderLineItemDto navisionLineItem = new NavisionOrderLineItemDto(
           supplyingFacility.getCode(),
           program.getCode(),
-          order.getCreatedDate().toString(),
+          processingPeriod.getEndDate().toString(),
           order.getId(),
           order.getOrderCode(),
           order.getEmergency(),
@@ -96,17 +115,16 @@ public class NavisionOrderCreator implements OrderCreatePostProcessor {
           orderablesMap.get(orderLineItem.getOrderable().getId()).getFullProductName(),
           orderablesMap.get(orderLineItem.getOrderable().getId()).getDispensable().getDisplayUnit(),
           orderLineItem.getOrderedQuantity(),
-          ""
+          lineItemsMap.get(orderLineItem.getOrderable().getId()).getRequestedQuantityExplanation()
       );
       request.add(navisionLineItem);
     }
 
-    String url = "http://172.17.0.1:9999/Company('39')/ReportAndRequisition";
     try {
       XLOGGER.debug("Sending request");
       RequestHeaders headers;
       headers = RequestHeaders.init().setAuth("98c5faf9-05cc-4839-aebc-7677bd916d41");
-      URI uri = RequestHelper.createUri(url);
+      URI uri = RequestHelper.createUri(navisionUrl);
       HttpEntity<List<NavisionOrderLineItemDto>> entity = RequestHelper.createEntity(request, headers);
 
       restTemplate.postForObject(uri, entity, Object.class);
